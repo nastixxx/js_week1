2a92bb8b4a92593640a7bd7a377c3cc4d698cc10
nastixxx <124407427+nastixxx@users.noreply.github.com>
2023-05-06 14:21:39 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+// задача №1
+let x = 40000;
+let y = 22000;
+console.log(x+y);
+
+// задача №2
+let city = 'Владивосток ';
+let count = '2000';
+console.log(`${city + count}`);
+
+//задача №3
+let a = 128;
+let b = 254;
+let c = 137;
+let d = 201;
+console.log((a+b+c+d)/60);
+
+//задача №4
+console.log(2**5);
+
+//задача №5
+console.log("Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!");
+
+//задача №6
+const greetings = "Привет";
+const name = "Кот";
+console.log( `${greetings}, ${name}`);
+
+//задача №7
+let time;
+time = 34;
+console.log(time);
+console.log('Старт поездки. Осталось минут: ' + time);
+time = time-15;
+console.log('Немного сториз в соцсетях. Осталось минут: ' + time);
+time = time-10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву');
+
+//задача №8
+let fahrenheit=451;
+let celsius=(fahrenheit - 32) / 1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту - это ${celsius} градуса по Цельсию.`);
+
+
+
